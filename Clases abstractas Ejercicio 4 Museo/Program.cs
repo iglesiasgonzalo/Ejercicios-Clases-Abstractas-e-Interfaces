1e4789ef944c62b40ec6350a9ef78731b17a8bc9
Gonzalo Iglesias <100100996+iglesiasgonzalo@users.noreply.github.com>
2025-06-22 18:12:55 -0300

    Add files via upload
    Clases Abstractas Ejercicio 4

---
+++ Clases abstractas Ejercicio 4 Museo/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_abstractas_Ejercicio_4_Museo
+{
+    internal class Program
+    {
+        abstract class Animal
+        {
+            private string nombre;
+
+            public void SetNombre(string nombre)
+            {
+                this.nombre = nombre;
+            }
+
+            public string GetNombre()
+            {
+                return nombre;
+            }
+
+            public abstract void HacerSonido();
+
+            public void MostrarNombre()
+            {
+                Console.WriteLine($"Animal: {GetNombre()}");
+            }
+        }
+
+        class Leon : Animal
+        {
+            public Leon()
+            {
+                SetNombre("León");
+            }
+
+            public override void HacerSonido()
+            {
+                Console.WriteLine("El león ruge: ¡GRAAAARRR!");
+            }
+        }
+
+        class Loro : Animal
+        {
+            public Loro()
+            {
+                SetNombre("Loro");
+            }
+
+            public override void HacerSonido()
+            {
+                Console.WriteLine("El loro dice: ¡Hola! ¿Querés una semilla?");
+            }
+        }
+
+        class Delfin : Animal
+        {
+            public Delfin()
+            {
+                SetNombre("Delfín");
+            }
+
+            public override void HacerSonido()
+            {
+                Console.WriteLine("El delfín emite un clic: *clic clic clic*");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Animal[] animales = new Animal[3];
+            animales[0] = new Leon();
+            animales[1] = new Loro();
+            animales[2] = new Delfin();
+
+            foreach (Animal animal in animales)
+            {
+                animal.MostrarNombre();
+                animal.HacerSonido();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+ 
